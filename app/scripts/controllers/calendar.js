676ef4ef32b5296078b6b7089c56ec4e3640be41
Pablo <pablocordoba11@gmail.com>
2015-11-14 01:28:08 -0300

    Corregido error __id

--- app/scripts/controllers/calendar.js
+++ app/scripts/controllers/calendar.js
@@ -2,18 +2,17 @@
 
 angular.module('dashboardApp')
 .controller('calendarCtrl', function($scope, $compile, $timeout, uiCalendarConfig) {
-  $scope.events=[]
+
     var date = new Date();
     var d = date.getDate();
     var m = date.getMonth();
     var y = date.getFullYear();
 
     /* event source that contains custom events on the scope */
+    $scope.events=[];
     $scope.events = [
       {title: 'All Day Event',start: new Date(y, m, 1)},
       {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
       {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
       {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
     ];
@@ -120,6 +119,6 @@ angular.module('dashboardApp')
       }
     };
     /* event sources array*/
-    $scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
-    $scope.eventSources2 = [$scope.calEventsExt, $scope.eventsF, $scope.events];
+    $scope.eventSources = [$scope.events];
+    //$scope.eventSources2 = [$scope.calEventsExt, $scope.eventsF, $scope.events];
 });
