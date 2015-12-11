b2c0c17ed5326b60634b6deab7d4fa31fe16328e
Pablo <pablocordoba11@gmail.com>
2015-12-11 13:22:00 -0300

    Reportes

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -37,6 +37,8 @@ module.exports = function(config) {
       'bower_components/lodash/lodash.js',
       'bower_components/angular-google-maps/dist/angular-google-maps.js',
       'bower_components/underscore/underscore.js',
+      'bower_components/Chart.js/Chart.js',
+      'bower_components/angular-chart.js/dist/angular-chart.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
